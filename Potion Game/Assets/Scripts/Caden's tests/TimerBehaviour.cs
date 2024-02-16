1e5733648788d365a6eb68983b771e0d3364a4c2
caden <cpete.est2014@gmail.com>
2024-02-16 13:20:40 -0700

    timer with text display

--- Potion Game/Assets/Scripts/Caden's tests/TimerBehaviour.cs
+++ Potion Game/Assets/Scripts/Caden's tests/TimerBehaviour.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
@@ -8,6 +9,29 @@ public class TimerBehaviour : MonoBehaviour
     public UnityEvent countdownStart, updateText, onCountdownFinished;
     public FloatData countdown;
     public float timeRemaining;
+    
+    public TMP_Text timerText;
+
+    private int minutes;
+    private int seconds;
+
+    private void Update() 
+    {
+        if(timeRemaining > 0)
+        {
+
+            minutes = Mathf.FloorToInt(timeRemaining / 60);
+            seconds = Mathf.FloorToInt(timeRemaining % 60);
+
+            timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+        else
+        {
+            // Time expired
+            timerText.text = "00:00";
+        }
+    }
+
     public void StartCountdown()
     {
         timeRemaining = countdown.data;
