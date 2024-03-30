264dc486eec86e8fffb44b8896f94131ed43af1a
Caden Peterson <10867828@uvu.edu>
2024-03-30 12:01:46 -0600

    recipes

--- Potion Game/Assets/Scripts/IDListBehaviour.cs
+++ Potion Game/Assets/Scripts/IDListBehaviour.cs
@@ -5,4 +5,25 @@ using UnityEngine;
 public class IDListBehaviour : MonoBehaviour
 {
    public IDList listObj;
+   public List<IDList> idListsToChooseFrom = new List<IDList>();
+   
+   public void SelectRandomIDList()
+   {
+      if (idListsToChooseFrom.Count > 0)
+      {
+         int randomIndex = Random.Range(0, idListsToChooseFrom.Count);
+         listObj = idListsToChooseFrom[randomIndex];
+      }
+      else
+      {
+         Debug.LogWarning("No IDLists found in the list! Please add some IDLists to the list.");
+      }
+   }
+
+   public IDList GetSelectedIDList()
+   {
+      return listObj;
+   }
+   
+   
 }
