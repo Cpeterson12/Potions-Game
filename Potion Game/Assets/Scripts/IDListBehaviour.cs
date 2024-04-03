2ae23b0dca76f6c3fe7128af705e292821fff583
Caden Peterson <10867828@uvu.edu>
2024-04-03 16:21:51 -0600

    Scene overhaul, Recipe Tickets, and more ingredient GOs

--- Potion Game/Assets/Scripts/IDListBehaviour.cs
+++ Potion Game/Assets/Scripts/IDListBehaviour.cs
@@ -1,18 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class IDListBehaviour : MonoBehaviour
 {
    public IDList listObj;
    public List<IDList> idListsToChooseFrom = new List<IDList>();
    
+   public TMP_Text idTextbox;
+
+   public void Awake()
+   {
+      UpdateIDTextbox();
+   }
+
    public void SelectRandomIDList()
    {
       if (idListsToChooseFrom.Count > 0)
       {
          int randomIndex = Random.Range(0, idListsToChooseFrom.Count);
          listObj = idListsToChooseFrom[randomIndex];
+         UpdateIDTextbox();
       }
       else
       {
@@ -24,6 +35,25 @@ public class IDListBehaviour : MonoBehaviour
    {
       return listObj;
    }
+   
+   private void UpdateIDTextbox()
+   {
+      if (listObj != null && listObj.list.Count > 0)
+      {
+         List<string> idNames = new List<string>();
+         foreach (ID id in listObj.list)
+         {
+            idNames.Add(id.name);
+         }
+         string idText = string.Join(", ", idNames);
+         idTextbox.text = idText;
+      }
+      else
+      {
+         idTextbox.text = "No IDs found.";
+      }
+   }
+
    
    
 }
